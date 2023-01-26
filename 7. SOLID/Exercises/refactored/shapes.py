5b0196041c896e066fea596787f23d91db2c903f
TomaMishev <47216444+TomaMishev@users.noreply.github.com>
2023-01-26 19:28:22 +0200

    Add files via upload

---
+++ 7. SOLID/Exercises/refactored/shapes.py
@@ -0,0 +1,47 @@
+# You are provided with code containing class Rectangle and class AreaCalculator. Refactor the code using the
+# Open/Closed Principle so that the code is open for extension (adding more shapes) but closed for modification.
+
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    @abstractmethod
+    def calc_area(self):
+        pass
+
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def calc_area(self):
+        return self.width * self.height
+
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def calc_area(self):
+        return self.side * self.side
+
+
+
+class AreaCalculator:
+    def __init__(self, shapes):
+        assert isinstance(shapes, list), "`shapes` should be of type `list`."
+        self.shapes = shapes
+
+    @property
+    def total_area(self):
+        total = 0
+        for shape in self.shapes:
+            total += shape.calc_area()
+
+        return total
+
+
+shapes = [Rectangle(2, 3), Rectangle(1, 6), Square(3)]
+calculator = AreaCalculator(shapes)
+print("The total area is: ", calculator.total_area)
