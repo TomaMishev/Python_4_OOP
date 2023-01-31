189cd9bf127d8cff78d1680390e020dabcd2ff20
TomaMishev <47216444+TomaMishev@users.noreply.github.com>
2023-01-31 11:59:53 +0200

    Add files via upload

---
+++ 9. Decorators/Lab/P02_vowel_filter.py
@@ -0,0 +1,15 @@
+def vowel_filter(function):
+
+    def wrapper():
+        result = [x for x in function() if x.lower() in "aeiou"]
+        return result
+
+    return wrapper
+
+
+
+@vowel_filter
+def get_letters():
+    return ["a", "b", "c", "d", "e"]
+
+print(get_letters())
